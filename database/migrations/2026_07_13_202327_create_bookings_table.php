c827cdc0d97f9ec25996a6e58c2bc38958299dec
riham-dev025 <harbriham96@gmail.com>
2026-07-14 00:02:57 +0300

    Initial commit with initial migrations

---
+++ database/migrations/2026_07_13_202327_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('user_id')->constrained('users');
+            // $table->foreignId('service_id')->constrained('services');
+            // $table->foreignId('resource_id')->constrained('resources');
+            // $table->foreignId('staff_id')->constrained('staff');
+            $table->timestamp('booking_start');
+            $table->timestamp('booking_end');
+            $table->integer('guests');
+            $table->string('booking_status', 20);
+            $table->string('payment_method', 50);
+            $table->string('payment_status', 20);
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
